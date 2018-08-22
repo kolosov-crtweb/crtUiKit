90da69267a3f098c6805db3a0f79b9aac5a06c02
Kolosov Oleg <kol@crtweb.ru>
2018-08-22 16:32:15 +0500

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var pug = require('gulp-pug');
+var sass = require('gulp-sass');
+
+gulp.task('sync', function (cb) {
+    setTimeout(function () {
+        cb();
+    }, 1000);
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./src/**/*.scss')
+        .pipe(sass.sync().on('error', sass.logError))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('pug', ['sync', 'sass'], function buildHTML() {
+    return gulp.src('src/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('dist'));
+});
+gulp.task('default', ['sass','pug']);
